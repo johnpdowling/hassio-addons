bd7534901ea25e53f0bdc34b16c2149ad4b812b7
John Dowling <john.patrick.dowling@gmail.com>
2019-05-26 11:39:23 -0700

    Update run.sh

--- fff-api/run.sh
+++ fff-api/run.sh
@@ -6,7 +6,7 @@ WAIT_TIME=10
 
 # Run fff-api
 while true; do
-    answer="$(cd /fff-api && FLASK_APP=webapi.py flask run --host=0.0.0.0 --port=5000)" || true
+    answer="$(cd /fff-api/api && FLASK_APP=webapi.py flask run --host=0.0.0.0 --port=5000)" || true
     
     echo "$(date): $answer"
     
