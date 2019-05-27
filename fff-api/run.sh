08826e7d85a1ba981ac5412c2ad54e0bc9ae74d2
John Dowling <john.patrick.dowling@gmail.com>
2019-05-26 18:56:47 -0700

    Update run.sh

--- fff-api/run.sh
+++ fff-api/run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
+CONFIG_PATH=/data/options.json
 
 # configs
+LOCAL_ONLY=$(jq --raw-output '.local_only' $CONFIG_PATH)
 WAIT_TIME=10
+
+HOST=""
+if [ !LOCAL_ONLY ]
+then
+HOST="--host=0.0.0.0"
+fi
 
 # Run fff-api
 while true; do
-    answer="$(cd /fff-api/api && FLASK_APP=webapi.py flask run --host=0.0.0.0 --port=5000)" || true
+    answer="$(cd /fff-api/api && FLASK_APP=webapi.py flask run $HOST --port=5000)" || true
     
     echo "$(date): $answer"
     
