6918233b90fe44cdeb385a4b28c1f09ee036984b
John Dowling <john.patrick.dowling@gmail.com>
2019-05-27 00:58:17 -0700

    Create run.sh

---
+++ kegbot/run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -ex
+
+# Run script for kegbot appserver.
+
+die() {
+  echo "Error: $@"
+  exit 1
+}
+
+do_mysql() {
+  mysql -B -u "${KEGBOT_DB_USER}" --password="${KEGBOT_DB_PASS}" -h "${KEGBOT_DB_HOST}" -P ${KEGBOT_DB_PORT} "${@}"
+  return $?
+}
+
+### Main routines
+
+setup_env() {
+
+  # Set defaults
+  if [ -z "${KEGBOT_DB_NAME}" ]; then
+    export KEGBOT_DB_NAME="kegbot"
+  fi
+  if [ -z "${KEGBOT_DB_USER}" ]; then
+    export KEGBOT_DB_USER="root"
+  fi
+  if [ -z "${KEGBOT_DB_PASS}" ]; then
+    export KEGBOT_DB_PASS=""
+  fi
+  if [ -z "${KEGBOT_DB_PORT}" ]; then
+    export KEGBOT_DB_PORT=3306
+  fi
+
+  if [ -z "${KEGBOT_REDIS_PORT}" ]; then
+    export KEGBOT_REDIS_PORT=6379
+  fi
+
+  if [ -z "${KEGBOT_SETTINGS_DIR}" ]; then
+    export KEGBOT_SETTINGS_DIR=/etc/kegbot/
+  fi
+
+  # Verify mandatory variables.
+  if [ -z "${KEGBOT_DB_HOST}" ]; then
+    die "Must set KEGBOT_DB_HOST or MYSQL_PORT_3306_TCP_{ADDR,PORT}"
+  fi
+  if [ -z "${KEGBOT_REDIS_HOST}" ]; then
+    die "Must set KEGBOT_REDIS_HOST or REDIS_PORT_6379_TCP_{ADDR,PORT}"
+  fi
+
+  export C_FORCE_ROOT=True   ## needed by celery
+
+  env
+}
+
+wait_for_mysql() {
+  nc -z $KEGBOT_DB_HOST $KEGBOT_DB_PORT || sleep 30
+  if ! do_mysql "${KEGBOT_DB_NAME}" -e "show tables"; then
+    do_mysql -e "create database ${KEGBOT_DB_NAME};"
+    kegbot migrate --noinput -v 0
+    do_mysql "${KEGBOT_DB_NAME}" -e "show tables"
+  fi
+}
+
+wait_for_redis() {
+  redis-cli -h "${KEGBOT_REDIS_HOST}" -p ${KEGBOT_REDIS_PORT} ping
+}
+
+# Perform first-launch setup.
+maybe_setup_kegbot() {
+  kegbot collectstatic --noinput -v 0
+  #do_mysql -e "create database ${KEGBOT_DB_NAME};" || die "Could not create database."
+  true
+}
+
+run_daemons() {
+  kegbot run_all --logs_dir=/kegbot-data --gunicorn_options="-b 0.0.0.0:8000"
+}
+
+run_all() {
+  setup_env
+
+  wait_for_mysql
+  #wait_for_redis
+
+  maybe_setup_kegbot
+  ls -ld /kegbot-data
+  ls -l /kegbot-data
+  echo `date` >> /kegbot-data/runlog
+  run_daemons
+}
+
+run_all
