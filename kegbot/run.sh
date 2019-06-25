0338b91c4338c36eae6d4440fa2fced5d4fa05ff
John Dowling <john.patrick.dowling@gmail.com>
2019-06-24 20:35:54 -0700

    Update run.sh

--- kegbot/run.sh
+++ kegbot/run.sh
@@ -25,28 +25,14 @@ setup_env() {
   export KEGBOT_DEBUG=$(jq --raw-output '.debug' $CONFIG_PATH)
   
   #pull optional options
-  if [ ! -z "$(jq --raw-output '.email_from' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_FROM=$(jq --raw-output '.email_from' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_host' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_HOST=$(jq --raw-output '.email_host' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_port' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_PORT=$(jq --raw-output '.email_port' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_user' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_USER=$(jq --raw-output '.email_user' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_pass' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_PASSWORD=$(jq --raw-output '.email_pass' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_ssl' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_USE_SSL=$(jq --raw-output '.email_ssl' $CONFIG_PATH)
-  fi
-  if [ ! -z "$(jq --raw-output '.email_tls' $CONFIG_PATH)"]; then
-    export KEGBOT_EMAIL_USE_TLS=$(jq --raw-output '.email_tls' $CONFIG_PATH)
-  fi
-
+  export KEGBOT_EMAIL_FROM=$(jq --raw-output '.email_from' $CONFIG_PATH)
+  export KEGBOT_EMAIL_HOST=$(jq --raw-output '.email_host' $CONFIG_PATH)
+  export KEGBOT_EMAIL_PORT=$(jq --raw-output '.email_port' $CONFIG_PATH)
+  export KEGBOT_EMAIL_USER=$(jq --raw-output '.email_user' $CONFIG_PATH)
+  export KEGBOT_EMAIL_PASSWORD=$(jq --raw-output '.email_password' $CONFIG_PATH)
+  export KEGBOT_EMAIL_USE_SSL=$(jq --raw-output '.email_use_ssl' $CONFIG_PATH)
+  export KEGBOT_EMAIL_USE_TLS=$(jq --raw-output '.email_use_tls' $CONFIG_PATH)
+ 
   # Set defaults to required
   if [ -z "${KEGBOT_DB_NAME}" ]; then
     export KEGBOT_DB_NAME="kegbot"
