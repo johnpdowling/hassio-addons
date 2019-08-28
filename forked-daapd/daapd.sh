bd94a3a7c660bccfecdce53d0ec0b38da0be6f9f
John Dowling <john.patrick.dowling@gmail.com>
2019-08-28 16:53:04 -0700

    Update daapd.sh

--- forked-daapd/daapd.sh
+++ forked-daapd/daapd.sh
@@ -9,7 +9,9 @@ dbus-daemon --system
 
 avahi-daemon --daemonize --no-chroot
 
-mkdir -p /config/cache
-[[ ! -f /config/forked-daapd.conf ]] && cp /usr/local/etc/forked-daapd.conf /config/forked-daapd.conf
+mkdir -p /config/forked-daapd/cache
+[[ ! -f /config/forked-daapd/forked-daapd.conf ]] && cp /usr/local/etc/forked-daapd.conf /config/forked-daapd/forked-daapd.conf
+mkdir -p /config/forked-daapd/music
+[[ ! -f /config/forked-daapd/music/HomeAssistantAnnounce ]] && mkfifo --mode=666 /config/forked-daapd/music/HomeAssistantAnnounce
 
-exec forked-daapd -f -c /config/forked-daapd.conf -P /var/run/forked-daapd.pid
+exec forked-daapd -f -c /config/forked-daapd/forked-daapd.conf -P /var/run/forked-daapd.pid
