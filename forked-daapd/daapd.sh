feb75a3eadafaa28b77cf168f90e1a2b513918be
John Dowling <john.patrick.dowling@gmail.com>
2019-08-30 09:27:06 -0700

    Update daapd.sh

--- forked-daapd/daapd.sh
+++ forked-daapd/daapd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #adapted from kevineye
-rm -rf /var/run
-mkdir -p /var/run/dbus
-dbus-uuidgen --ensure
-sleep 1
-dbus-daemon --system
+#rm -rf /var/run
+#mkdir -p /var/run/dbus
+#dbus-uuidgen --ensure
+#sleep 1
+#dbus-daemon --system
 
 avahi-daemon --daemonize --no-chroot
 
