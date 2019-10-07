cb8142df5f61cd337931b0fa41ad2efc73e6f673
John Dowling <john.patrick.dowling@gmail.com>
2019-10-07 00:09:06 -0700

    Update keras-rest.sh

--- keras-rest/rootfs/etc/cont-init.d/keras-rest.sh
+++ keras-rest/rootfs/etc/cont-init.d/keras-rest.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
 # JPD Hass.io Add-ons: Keras REST Server
-# Configures the things
+# Runs the Keras model saver
 # ==============================================================================
+set -e
+CONFIG_PATH=/data/options.json
+
+#pull required option(s)
+export MODEL_NAME=$(jq --raw-output '.model_name' $CONFIG_PATH)
+if [ -z "${MODEL_NAME}" ]; then
+    export MODEL_NAME="imagenet"
+fi
+bashio::log.debug "Setting up Keras model..."
+python3 /app/modelsaver/main.py
