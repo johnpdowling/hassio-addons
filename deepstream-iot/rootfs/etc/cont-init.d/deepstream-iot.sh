5dfd7d2a2d090d36afa20e5fc33a645475ff05bd
John Dowling <john.patrick.dowling@gmail.com>
2020-02-17 20:09:29 -0800

    Update deepstream-iot.sh

--- deepstream-iot/rootfs/etc/cont-init.d/deepstream-iot.sh
+++ deepstream-iot/rootfs/etc/cont-init.d/deepstream-iot.sh
@@ -2,5 +2,5 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
 # JPD Hass.io Add-ons: DeepStream IoT Server
-# Runs the deepstream-app
+# Runs any config
 # ==============================================================================
