bf8d63113246c68b6f3d35cb26a57a479df45aae
John Dowling <john.patrick.dowling@gmail.com>
2020-02-25 02:07:27 -0800

    Create wlanstart.sh

---
+++ hass-ap/rootfs/etc/cont-init.d/wlanstart.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# JPD Hass.io Add-ons: HASS-AP
+# Configures all the scripts
+# ==============================================================================
+
+# no need to check if we're in privileged mode, set in config.json
+
+#CONFIG_PATH=/data/options.json
+#MQTT_HOST="$(jq --raw-output '.mqtt_host' $CONFIG_PATH)"
+
+# Default values
+true ${INTERFACE:=wlan0}
+true ${SUBNET:=192.168.254.0}
+true ${AP_ADDR:=192.168.254.1}
+true ${SSID:=dockerap}
+true ${CHANNEL:=11}
+true ${WPA_PASSPHRASE:=passw0rd}
+true ${HW_MODE:=g}
+true ${DRIVER:=nl80211}
+true ${HT_CAPAB:=[HT40-][SHORT-GI-20][SHORT-GI-40]}
+true ${MODE:=host}
+
+# Attach interface to container in guest mode
+if [ "$MODE" == "guest"  ]; then
+    echo "Attaching interface to container"
+
+    CONTAINER_ID=$(cat /proc/self/cgroup | grep -o  -e "/docker/.*" | head -n 1| sed "s/\/docker\/\(.*\)/\\1/")
+    CONTAINER_PID=$(docker inspect -f '{{.State.Pid}}' ${CONTAINER_ID})
+    CONTAINER_IMAGE=$(docker inspect -f '{{.Config.Image}}' ${CONTAINER_ID})
+
+    docker run -t --privileged --net=host --pid=host --rm --entrypoint /bin/sh ${CONTAINER_IMAGE} -c "
+        PHY=\$(echo phy\$(iw dev ${INTERFACE} info | grep wiphy | tr ' ' '\n' | tail -n 1))
+        iw phy \$PHY set netns ${CONTAINER_PID}
+    "
+
+    ip link set ${INTERFACE} name wlan0
+
+    INTERFACE=wlan0
+fi
+
+if [ ! -f "/etc/hostapd.conf" ] ; then
+    cat > "/etc/hostapd.conf" <<EOF
+interface=${INTERFACE}
+driver=${DRIVER}
+ssid=${SSID}
+hw_mode=${HW_MODE}
+channel=${CHANNEL}
+wpa=2
+wpa_passphrase=${WPA_PASSPHRASE}
+wpa_key_mgmt=WPA-PSK
+# TKIP is no secure anymore
+#wpa_pairwise=TKIP CCMP
+wpa_pairwise=CCMP
+rsn_pairwise=CCMP
+wpa_ptk_rekey=600
+ieee80211n=1
+ht_capab=${HT_CAPAB}
+wmm_enabled=1 
+EOF
+
+fi
+
+# unblock wlan
+rfkill unblock wlan
+
+echo "Setting interface ${INTERFACE}"
+
+# Setup interface and restart DHCP service 
+ip link set ${INTERFACE} up
+ip addr flush dev ${INTERFACE}
+ip addr add ${AP_ADDR}/24 dev ${INTERFACE}
+
+# NAT settings
+echo "NAT settings ip_dynaddr, ip_forward"
+
+for i in ip_dynaddr ip_forward ; do 
+  if [ $(cat /proc/sys/net/ipv4/$i) ]; then
+    echo $i already 1 
+  else
+    echo "1" > /proc/sys/net/ipv4/$i
+  fi
+done
+
+cat /proc/sys/net/ipv4/ip_dynaddr 
+cat /proc/sys/net/ipv4/ip_forward
+
+if [ "${OUTGOINGS}" ] ; then
+   ints="$(sed 's/,\+/ /g' <<<"${OUTGOINGS}")"
+   for int in ${ints}
+   do
+      echo "Setting iptables for outgoing traffics on ${int}..."
+      iptables -t nat -A POSTROUTING -o ${int} -j MASQUERADE
+      iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+      iptables -A FORWARD -i ${INTERFACE} -o ${int} -j ACCEPT
+   done
+else
+   echo "Setting iptables for outgoing traffics on all interfaces..."
+   iptables -t nat -A POSTROUTING -j MASQUERADE
+   iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+   iptables -A FORWARD -i ${INTERFACE} -j ACCEPT
+fi
+echo "Configuring DHCP server .."
+
+if [ ! -f "/config/dhcp-reservations.conf" ] ; then
+  echo "" > /config/dhcp-reservations.conf
+fi
+
+cat > "/etc/dhcp/dhcpd.conf" <<EOF
+option domain-name-servers 8.8.8.8, 8.8.4.4;
+option subnet-mask 255.255.255.0;
+option routers ${AP_ADDR};
+subnet ${SUBNET} netmask 255.255.255.0 {
+  range ${SUBNET::-1}100 ${SUBNET::-1}199;
+}
+include "/config/dhcp-reservations.conf";
+EOF
