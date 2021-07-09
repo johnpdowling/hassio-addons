96dbfa116a9228a449c06632302fa2fd00e7861d
John Dowling <john.patrick.dowling@gmail.com>
2021-07-08 22:57:35 -0700

    Update owntone.sh

--- owntone/rootfs/etc/cont-init.d/owntone.sh
+++ owntone/rootfs/etc/cont-init.d/owntone.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.debug 'holy shit we bash-ee-oh-d'
-bashio::log.info 'look ma we made it'
+#Start it up
+
+if ! bashio::fs.directory_exists '/config/owntone/cache'; then
+    bashio::log.debug 'Creating cache folder...'
+    mkdir -p /config/owntone/cache
+fi
+
+if ! bashio::fs.file_exists '/config/forked-daapd/forked-daapd.conf'; then
+    bashio::log.debug 'Copying default conf file...'
+    cp /usr/local/etc/owntone.conf /config/owntone/owntone.conf
+fi
+
+if ! bashio::fs.directory_exists '/config/owntone/music'; then
+bashio::log.debug 'Creating music folder...'
+    mkdir -p /config/owntone/music
+    bashio::log.debug 'Creating HA fifo file...'
+    mkfifo -m 666 /config/owntone/music/HomeAssistantAnnounce
+fi
+
