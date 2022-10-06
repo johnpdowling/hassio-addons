f59b57b6d15a77cafed69f6f6b742a3cfb1162e6
John Dowling <john.patrick.dowling@gmail.com>
2022-10-05 17:35:17 -0700

    log info

--- owntone/rootfs/etc/cont-init.d/owntone.sh
+++ owntone/rootfs/etc/cont-init.d/owntone.sh
@@ -3,19 +3,19 @@
 #Start it up
 
 if ! bashio::fs.directory_exists '/config/owntone/cache'; then
-    bashio::log.debug 'Creating cache folder...'
+    bashio::log.info 'Creating cache folder...'
     mkdir -p /config/owntone/cache
 fi
 
 if ! bashio::fs.file_exists '/config/owntone/owntone.conf'; then
-    bashio::log.debug 'Copying default conf file...'
+    bashio::log.info 'Copying default conf file...'
     cp /usr/local/etc/owntone.conf /config/owntone/owntone.conf
 fi
 
 if ! bashio::fs.directory_exists '/config/owntone/music'; then
-    bashio::log.debug 'Creating music folder...'
+    bashio::log.info 'Creating music folder...'
     mkdir -p /config/owntone/music
-    bashio::log.debug 'Creating HA fifo file...'
+    bashio::log.info 'Creating HA fifo file...'
     mkfifo -m 666 /config/owntone/music/HomeAssistantAnnounce
 fi
 
