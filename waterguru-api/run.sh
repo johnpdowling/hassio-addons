6a783e3f206b464ae9fe57aa3c2c28e18b805489
John Dowling <john.patrick.dowling@gmail.com>
2023-12-02 23:18:12 -0800

    Create run.sh
    Initial merge of waterguru-api Dockerfile commands + run.sh with example add-on run.sh

---
+++ waterguru-api/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+bashio::log.info "Creating WG configuration..."
+
+# Create main config
+WG_USER=$(bashio::config 'user')
+WG_PASS=$(bashio::config 'password')
+
+sed -i "s/WG_USER/${WG_USER}/" /waterguru_flask.py
+sed -i "s/WG_PASS/${WG_PASS}/" /waterguru_flask.py
+sed -i "s/WG_PORT/${WG_PORT}/" /waterguru_flask.py
+
+bashio::log.info "Starting WG API server..."
+python3 /waterguru_flash.py
